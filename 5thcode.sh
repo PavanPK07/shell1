bc2f051be3267265aa5767ce1ebc34bc5a744380
PavanPK07 <kalyanreddypallala@gmail.com>
2024-02-12 00:22:17 +0530

    Variables learning

---
+++ 5thcode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter username::"
+read -s
+
+echo "Please enter password::"
+read -s 
+
+echo "username is $USERNAME and password is $PASSWORD"
+#just for validation we should neve print that kind of sensitive information
