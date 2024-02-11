a13920dfa56ff3a2b4f56cf7f8afb7ebe8a3cae4
PavanPK07 <kalyanreddypallala@gmail.com>
2024-02-11 23:42:58 +0530

    I have written my 2nd code

---
+++ 2ndcode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Pavan
+PERSON2=Kalyan
+
+echo"$PERSON1: Hi $PERSON2, Good morning"
+echo"$PERSON2: Hi $PERSON1, Good morning"
+echo"$PERSON1: How are you $PERSON2"
+echo"$PERSON2: I am fine, How are you $PERSON1"
+echo"$PERSON1: Iam fine"
