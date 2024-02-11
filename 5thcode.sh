be8b0bad2b71e653c897a880bf2c3138b6316d6f
PavanPK07 <kalyanreddypallala@gmail.com>
2024-02-12 00:24:42 +0530

    I am rectifying a mistake in there

--- 5thcode.sh
+++ 5thcode.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Please enter username::"
-read -s
+read -s USERNAME
 
 echo "Please enter password::"
-read -s 
+read -s PASSWORD
 
 echo "username is $USERNAME and password is $PASSWORD"
 #just for validation we should neve print that kind of sensitive information
