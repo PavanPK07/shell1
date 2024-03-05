6794fbb724926e8da7fb0f4b7bdfab9d06bedecd
PavanPK07 <kalyanreddypallala@gmail.com>
2024-03-05 15:11:37 +0530

    checking disk usage

---
+++ disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df-hT | grep -vE "tmp|file")
+DISK_THRESOLD=1
+message=""
+
+while IFS= read -r line
+do
+    usage=$(echo"$line | awk '{print 6F}' | cut -d % -f1")
+    partition=$(echo"$line | awk '{print 1F}' ")
+    if ($usage -ge $DISK_THRESOLD)
+    then
+        mesaage+="High disk usage detected on $partition: $usage"
+    fi
+done <<<$DISK_USAGE
+
+echo "message"
+
+
