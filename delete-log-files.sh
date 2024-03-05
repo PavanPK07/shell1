2c07779edf52bfcc06ea9182accd45ca566da17e
PavanPK07 <kalyanreddypallala@gmail.com>
2024-03-05 14:02:05 +0530

    I am deleting log files that are older than 14 days

---
+++ delete-log-files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE_DIR=/temp/shellscript-logs
+
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N="/E[0m"
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$R Source dir $SOURCE_DIR does not exist $N"
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR type -f mtime 14 name "*.log")
+
+while IFS read -r line
+do
+    echo "deleting file $line"
+    rm -rf $line
+done <<<$FILES_TO_DELETE
